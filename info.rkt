4d9ffa17b96864afa33a26966e13f9d6c58639b7
David Van Horn <dvanhorn@cs.umd.edu>
2024-11-13 23:29:39 -0500

    Omit compiler tests in info.rkt.

--- info.rkt
+++ info.rkt
@@ -3,3 +3,5 @@
 (define collection 'multi)
 (define deps (list "base" "rackunit"
   "https://github.com/cmsc430/a86.git?path=#main"))
+
+(define test-omit-paths (list "knock-plus/test/compile.rkt"))
