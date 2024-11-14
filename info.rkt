95ed25baa217e9f300fd288164e569da8c6af11e
David Van Horn <dvanhorn@cs.umd.edu>
2024-11-14 10:41:11 -0500

    Skip iniquity-plus compiler tests.

--- info.rkt
+++ info.rkt
@@ -4,4 +4,7 @@
 (define deps (list "base" "rackunit"
   "https://github.com/cmsc430/a86.git?path=#main"))
 
-(define test-omit-paths (list "knock-plus/test/compile.rkt"))
+(define test-omit-paths
+  (list "iniquity-plus/test/compile.rkt"
+        "knock-plus/test/compile.rkt"))
+        
