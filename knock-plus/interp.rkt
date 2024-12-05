1c69a5a6914669839093e4fbc02875771aa25a44
dvanhorn <dvanhorn@cs.umd.edu>
2024-12-05 21:34:40 +0000

    crook

--- knock-plus/interp.rkt
+++ knock-plus/interp.rkt
@@ -4,6 +4,7 @@
 (provide interp-match-pat)
 (require "ast.rkt")
 (require "interp-prim.rkt")
+(require "env.rkt")
 
 ;; type Value =
 ;; | Integer
@@ -166,16 +167,4 @@
     [((cons x xs) (cons y ys))
      (cons (list x y)
            (zip xs ys))]))
-
-;; Env Id -> Value
-(define (lookup r x)
-  (match r
-    [(cons (list y val) r)
-     (if (symbol=? x y)
-         val
-         (lookup r x))]))
-
-;; Env Id Value -> Env
-(define (ext r x v)
-  (cons (list x v) r))
 
