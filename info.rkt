acbf6ab1bce602b74926370cf33f584d7b5050fb
David Van Horn <dvanhorn@cs.umd.edu>
2025-12-04 20:33:01 -0500

    Loot with exceptions project.

--- info.rkt
+++ info.rkt
@@ -7,5 +7,6 @@
 (define test-omit-paths
   (list "iniquity-plus/test/compile.rkt"
         "knock-plus/test/compile.rkt"
-        "hoax-plus/test/compile.rkt"))
+        "hoax-plus/test/compile.rkt"
+        "loot-exceptions/test/compile.rkt"))
         
