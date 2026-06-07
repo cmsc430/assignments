acfdb4f84df9c847172b3d2421d85193ccd58776
David Van Horn <dvanhorn@cs.umd.edu>
2026-06-07 08:15:47 -0400

    Add extort standalone runtime, Makefiles.

--- extort-plus/executor/exec.rkt
+++ extort-plus/executor/exec.rkt
@@ -1,12 +1,15 @@
 #lang racket
-(provide exec exec/io)
+(provide run exec exec/io)
 (require "../compiler/compile.rkt")
 (require "decode.rkt")
 (require "host.rkt")
+;; Asm -> Answer
+(define (run asm)
+  (with-handlers ([(λ (x) (eq? x 'err)) identity])
+    (bits->value (asm-interp/host asm))))
 ;; Expr -> Answer
 (define (exec e)
-  (with-handlers ([(λ (x) (eq? x 'err)) identity])
-    (bits->value (asm-interp/host (compile e)))))
+  (run (compile e)))
 ;; Asm String -> (cons Answer String)
 (define (exec/io asm in)
   (parameterize ((current-output-port (open-output-string))
